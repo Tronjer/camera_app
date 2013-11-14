9267ccd6f582d62014191772daf0822882758c1a
Tronjer <counteight@googlemail.com>
2013-11-14 22:45:31 +0100

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,16 @@ module ApplicationHelper
 
 	# returns the page title
 	def page_title(title)
-    base_title = "Tronjer"
-    if title.empty?
-      base_title
-    else
-      "#{base_title} | #{title}"
-    end
+		base_title = "Tronjer"
+		if title.empty?
+			base_title
+		else
+			"#{base_title} | #{title}"
+		end
 	end
 
 	# for namespacing in scss files
 	def body_classes
-    [controller.controller_name, controller.action_name].join(' ')
+		[controller.controller_name, controller.action_name].join(' ')
 	end
 end
