bc60ac8f19282774326b2471df19e6e5717c8068
counteight <matthias.kalden@me.com>
2013-11-14 23:12:46 +0100

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 
-	# returns the page title
-	def page_title(title)
-		base_title = "Tronjer"
-		if title.empty?
-			base_title
-		else
-			"#{base_title} | #{title}"
-		end
-	end
+  # returns the page title
+  def page_title(title)
+    base_title = "Tronjer"
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
 
-	# for namespacing in scss files
-	def body_classes
-		[controller.controller_name, controller.action_name].join(' ')
-	end
+  # for namespacing in scss files
+  def body_classes
+    [controller.controller_name, controller.action_name].join(' ')
+  end
 end
