65dd63cbf12409ed6b8c198e864d3848b9a4e235
Tronjer <counteight@googlemail.com>
2013-11-14 22:31:10 +0100

    Upgrade helper and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 
 	# returns the page title
-	def full_title(page_title)
-        base_title = "Tronjer"
-      	if page_title.empty?
-        	base_title
-        else
-        	"#{base_title} | #{page_title}"
-        end
+	def page_title(title)
+    base_title = "Tronjer"
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
 	end
 
 	# for namespacing in scss files
 	def body_classes
-        [controller.controller_name, controller.action_name].join(' ')
+    [controller.controller_name, controller.action_name].join(' ')
 	end
 end
